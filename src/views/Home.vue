04609fae915b80bedf303ecfc3731c1cc40cb24c
Luciano Baraúna <baraunaluciano@gmail.com>
2018-11-01 17:14:25 -0300

    test get api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>Respostas da API:</h1>
+    <h2>Corruptos</h2>
+    <p>{{dados}}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'home',
+  data () {
+    return {
+      dados: ''
+    }
+  },
   components: {
     HelloWorld
+  },
+  mounted () {
+    this.load()
+  },
+  methods: {
+    load () {
+      axios
+        .get('https://jarbas.serenata.ai/api/chamber_of_deputies/reimbursement/?state=RJ&year=2018')
+        .then((resp) => {
+          this.dados = resp
+        })
+    }
   }
 }
 </script>
