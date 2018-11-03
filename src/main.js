108da0f5b6eb71d9498cdc7453aaea31c96c7880
Luciano Baraúna <baraunaluciano@gmail.com>
2018-11-03 19:17:11 +0000

    implemented basic leaflet

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+import L from 'leaflet'
+import 'leaflet.icon.glyph'
+
 import App from './App.vue'
 import router from './router'
 import store from './store/'
 import './registerServiceWorker'
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: './assets/images/marker-icon-2x.png',
+  iconUrl: './assets/images/marker-icon.png',
+  shadowUrl: './assets/images/marker-shadow.png'
+})
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,55 @@
 <template>
   <div class="home">
-    <h1>Respostas da API:</h1>
+    <!-- <h1>Respostas da API:</h1>
     <h2>Corruptos</h2>
-    <pre>{{getReimbursementList}}</pre>
+    <pre>{{getReimbursementList}}</pre> -->
+    <l-map
+      :zoom="zoom"
+      :center="center"
+      style="height: 80%"
+      @update:center="centerUpdate"
+      @update:zoom="zoomUpdate">
+      <l-tile-layer
+        :url="url"
+        :attribution="attribution"/>
+      <l-marker :lat-lng="marker">
+        <l-popup>
+          <div @click="popupClick">
+            I am a tooltip
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi. Donec finibus semper metus id malesuada.
+            </p>
+          </div>
+        </l-popup>
+      </l-marker>
+    </l-map>
+
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import { LMap, LTileLayer, LMarker, LPopup } from 'vue2-leaflet'
 
 export default {
   name: 'home',
   data () {
     return {
-      dados: ''
+      zoom: 13,
+      center: L.latLng(47.413220, -1.219482),
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      marker: L.latLng(47.413220, -1.219482),
+      currentZoom: 13,
+      currentCenter: L.latLng(47.413220, -1.219482),
+      showParagraph: false
     }
   },
   components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup
   },
   mounted () {
     this.fetchReimbursementList()
@@ -25,7 +58,19 @@ export default {
     ...mapGetters('mapwinners', ['getReimbursementList'])
   },
   methods: {
-    ...mapActions('mapwinners', ['fetchReimbursementList'])
+    ...mapActions('mapwinners', ['fetchReimbursementList']),
+    zoomUpdate (zoom) {
+      this.currentZoom = zoom
+    },
+    centerUpdate (center) {
+      this.currentCenter = center
+    },
+    showLongText () {
+      this.showParagraph = !this.showParagraph
+    },
+    popupClick () {
+      alert('Popup Click!')
+    }
   }
 }
 </script>
