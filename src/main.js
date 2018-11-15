a519be030dbe326f15c583916daaccf9c9c5c54e
Luciano Baraúna <baraunaluciano@gmail.com>
2018-11-15 15:01:52 +0000

    remove comments

--- src/main.js
+++ src/main.js
@@ -4,16 +4,6 @@ import router from './router'
 import store from './store/'
 import './registerServiceWorker'
 
-//  LEAFLET
-// import L from 'leaflet'
-// delete L.Icon.Default.prototype._getIconUrl
-
-// L.Icon.Default.mergeOptions({
-//   iconRetinaUrl: './assets/images/marker-icon-2x.png',
-//   iconUrl: './assets/images/marker-icon.png',
-//   shadowUrl: './assets/images/marker-shadow.png'
-// })
-
 Vue.config.productionTip = false
 
 new Vue({
